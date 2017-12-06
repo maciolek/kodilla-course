4ea654e2ef429ebf5afe45cd8fa73398a777d6ad
“Maciolek <maciolek.dariusz@gmail.com>
2017-12-06 10:53:22 +0100

    create new module and package

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing;
+
+public class TestingMain {
+
+    public static void main(String[] args) {
+        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+    }
+
+}
