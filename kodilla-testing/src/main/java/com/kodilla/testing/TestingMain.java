0c88dcef63c4358f179942a4859d00b5ddf7c6d3
“Maciolek <maciolek.dariusz@gmail.com>
2017-12-09 23:20:20 +0100

    pierwsze poprawki PR

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -2,13 +2,17 @@ package com.kodilla.testing;
 import com.kodilla.testing.collection.OddNumbersExterminator;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static java.util.Arrays.asList;
 
 public class TestingMain {
 
     public static void main(String[] args) {
 
         ArrayList<Integer> numbers = new ArrayList<Integer>();
-        numbers.add(1);
+        Arrays.asList(1,2,3,4,10,11,12,13);
+/*        numbers.add(1);
         numbers.add(2);
         numbers.add(3);
         numbers.add(4);
@@ -17,7 +21,7 @@ public class TestingMain {
         numbers.add(12);
         numbers.add(13);
         OddNumbersExterminator packNumbers = new OddNumbersExterminator();
-        packNumbers.exterminate(numbers);
-        packNumbers.show();
+        packNumbers.exterminate(numbers);*/
+        System.out.println("wielkość: " + numbers.size());
     }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -5,27 +5,30 @@ import java.lang.*;
 
 
 public class OddNumbersExterminator {
-    ArrayList<Integer> listEven;
+  /*  ArrayList<Integer> listEven;
     public OddNumbersExterminator() {
         listEven = new ArrayList<Integer>();
-    }
-
+    }*/
     public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
-        for (Integer currentNumber : numbers)
-            if (currentNumber % 2 == 0) {
-                listEven.add(currentNumber);
+        for (Integer currentNumber : numbers) {
+            System.out.print(currentNumber);
+            if (currentNumber % 2 != 0) {
+                System.out.print(currentNumber);
+                numbers.remove(currentNumber);
+          //      System.out.print(numbers.get(currentNumber));
             }
-        return listEven;
+        }
+            return numbers;
     }
 
-    public void show() {
+ /*   public void show() {
         System.out.println("Liczby parzyste dodane do listy: ");
-        for (int i = 0; i < listEven.size(); i++) {
-            System.out.print(listEven.get(i));
+        for (int i = 0; i < numbers.size(); i++) {
+            System.out.print(numbers.get(i));
             System.out.print(" ");
         }
-    }
-    public ArrayList<Integer> getListEven() {
-        return listEven;
-    }
+    }*/
+ /*   public ArrayList<Integer> numbers() {
+        return this.numbers;
+    }*/
 }
