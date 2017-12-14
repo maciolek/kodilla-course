feade8b77525bd342658749f4680a1ae02f374f3
“Maciolek <maciolek.dariusz@gmail.com>
2017-12-14 01:39:03 +0100

    task 6.5 complete v1

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,45 +1,16 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.user.SimpleUser;
-
+import com.kodilla.testing.forum.statistics.CalculateStatistic;
+import com.kodilla.testing.forum.statistics.Statistics;
 
 public class TestingMain {
 
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
 
-        SimpleUser user = new SimpleUser("Maciej");
-
-        String test = user.getUserName();
-
-        if (test.equals("Maciej")) {
-            System.out.println("Test OK");
-        } else {
-            System.out.println("Test error!");
-        }
-// test modułu calculator
-
-        System.out.println("\nTest kalkulator - ZADANIE 6.2 \n ");
-        Calculate result = new Calculate(5, 5);
-
-        Integer numberOne = result.getNumberOne();
-        Integer numberTwo = result.getNumberTwo();
-
-        Integer addTest = result.addNumber();
-        Integer substTest = result.substractNumber();
-
-// test add
-        if (addTest.equals((numberOne + numberTwo))) {
-            System.out.println("Test dodawania OK");
-        } else {
-            System.out.println("Test dodawania error!");
-        }
-// test substract
-        if (substTest.equals((numberOne - numberTwo))) {
-            System.out.println("Test odejmowania OK");
-        } else {
-            System.out.println("Test odejmowania error!");
-        }
-
+ /*       Statistics objStat = new TestClassStub();
+        CalculateStatistic objCalculate = new CalculateStatistic(objStat);
+        objCalculate.calculateAdvStatistics(objStat);
+        objCalculate.showStatistics();
+    }*/
     }
 }
