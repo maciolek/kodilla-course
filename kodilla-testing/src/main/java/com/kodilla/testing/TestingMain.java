56ba2b04183c07e8e5fe92d4f1dd7698e4815aba
“Maciolek <maciolek.dariusz@gmail.com>
2017-12-15 23:51:37 +0100

    odtworzylem class CalculateStatistic

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,16 +1,8 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.forum.statistics.CalculateStatistic;
-import com.kodilla.testing.forum.statistics.Statistics;
-
 public class TestingMain {
 
     public static void main(String[] args) {
 
- /*       Statistics objStat = new TestClassStub();
-        CalculateStatistic objCalculate = new CalculateStatistic(objStat);
-        objCalculate.calculateAdvStatistics(objStat);
-        objCalculate.showStatistics();
-    }*/
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/CalculateStatistic.java
@@ -0,0 +1,61 @@
+package com.kodilla.testing.forum.statistics;
+
+import java.util.List;
+
+public class CalculateStatistic {
+
+    private Statistics statistics;
+    private int numbersOfUsers;
+    private int numberOfPosts;
+    private int numbersOfCommets;
+    private double averageNumbersCommentsOfPost;
+    private double averageNumbersCommentsOfUser;
+    private double averageNumbersPostOfUser;
+
+    public CalculateStatistic(Statistics statistics) {
+        this.statistics = statistics;
+    }
+
+    public void calculateAdvStatistics(Statistics statistics) {
+        numbersOfUsers = statistics.usersNames().size();
+        numberOfPosts =  statistics.postsCount();
+        numbersOfCommets = statistics.commentsCount();
+        if (numbersOfUsers == 0) {
+            averageNumbersPostOfUser = 0.0;
+            averageNumbersCommentsOfUser = 0.0;
+        } else {
+            averageNumbersPostOfUser = (double) numberOfPosts / (double) numbersOfUsers;
+            averageNumbersCommentsOfUser = (double) numbersOfCommets / (double) numbersOfUsers;
+        }
+        if (numberOfPosts == 0) {
+            averageNumbersCommentsOfPost = 0.0;
+        } else {
+            averageNumbersCommentsOfPost = (double)numbersOfCommets / (double)numberOfPosts;
+        }
+    }
+
+    public void showStatistics() {
+        System.out.println("Ilość użytkowników " + numbersOfUsers);
+        System.out.println("Ilość postów " + numberOfPosts);
+        System.out.println("Ilość komentarzy " + numbersOfCommets);
+        System.out.println("Srednia ilość postów na użytkownika " + averageNumbersPostOfUser);
+        System.out.println("Srednia ilość komentarzy na użytkownika " + averageNumbersCommentsOfUser);
+        System.out.println("Srednia ilość korzymenta na post " + averageNumbersCommentsOfPost);
+    }
+
+    public double getNumbersOfUsers() {
+        return numbersOfUsers;
+    }
+
+    public double getAverageNumbersCommentsOfPost() {
+        return averageNumbersCommentsOfPost;
+    }
+
+    public double getAverageNumbersCommentsOfUser() {
+        return averageNumbersCommentsOfUser;
+    }
+
+    public double getAverageNumbersPostOfUser() {
+        return averageNumbersPostOfUser;
+    }
+}
