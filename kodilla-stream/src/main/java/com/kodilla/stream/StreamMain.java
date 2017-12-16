83539e8b884d4d7142f4ea37c6e2c21b41c3ed5f
“Maciolek <maciolek.dariusz@gmail.com>
2017-12-17 00:41:08 +0100

    task 7.1 complete v1

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,12 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.MyBeautyfier;
 import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.lambda.ExecuteSaySomething;
-import com.kodilla.stream.lambda.Executor;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.lambda.Processor;
-import com.kodilla.stream.reference.FunctionalCalculator;
-import com.sun.org.apache.xpath.internal.SourceTree;
+
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -17,26 +12,5 @@ public class StreamMain {
         poemBeautifier.beautify("Nauka JAVY zajmuje mnóstwo czasu!", (text) -> MyBeautyfier.beautifierAddLineInHash(text));
         poemBeautifier.beautify("Umiem coraz wiecej", (text) -> MyBeautyfier.beautifierOneLineOneChar(text));
 
-/*// mod 7.1
-        Processor processor = new Processor();
-        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
-        processor.execute(executeSaySomething);
-// lambda 1 bez objektu klasy pośredniczącej!
-//        Executor executor = () -> System.out.println("0This is an example text - lambda 1");
-//        processor.executor(executor);
-//lambda 2 bez tworzenia zmiennej :-)
-//        processor.execute (()-> System.out.println("This is an example text - lambda 2"));
-//lambda z argumentami 3
-//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-//        expressionExecutor.executeExpression(5,5,  (a, b) -> a + b);
-//        expressionExecutor.executeExpression(10,8, (a, b) -> a - b);
-//        expressionExecutor.executeExpression(20,5, (a, b) -> a / b);
-//        expressionExecutor.executeExpression(3,3,(a, b) -> a * b);
-//lambda z referencjami 4
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
-        expressionExecutor.executeExpression(5,5, FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(10,8, FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(20,5, FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3,3,FunctionalCalculator::divideAByB);*/
     }
 }
