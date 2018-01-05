03f87bec0ea05b8a0a510cb45b571472a839d864
Dariusz <33531485+Maciolek@users.noreply.github.com>
2018-01-05 15:36:31 +0100

    Zadanie 7.2 (#7)
    add task7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,16 +1,15 @@
-package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.MyBeautyfier;
-import com.kodilla.stream.beautifier.PoemBeautifier;
-
+import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.lambda.ExecuteSaySomething;
+import com.kodilla.stream.lambda.Executor;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.lambda.Processor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+import com.sun.org.apache.xpath.internal.SourceTree;
 
 public class StreamMain {
     public static void main(String[] args) {
-
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("\nNauka JAVA to sama przyjemność! :-D",String::toUpperCase);
-        poemBeautifier.beautify("Nauka JAVY zajmuje mnóstwo czasu!", (text) -> MyBeautyfier.beautifierAddLineInHash(text));
-        poemBeautifier.beautify("Umiem coraz wiecej", (text) -> MyBeautyfier.beautifierOneLineOneChar(text));
-
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
