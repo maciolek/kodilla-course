afe2c08c976b691b6053a1ad501adda4c8d04fa3
“Maciolek <maciolek.dariusz@gmail.com>
2018-01-18 16:59:36 +0100

    10.2 Kalkulator z wyświetlaczem - done

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
@@ -0,0 +1,15 @@
+package com.kodilla.spring.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configuration
+public class Display {
+
+    public boolean displayValue(double val){
+        System.out.println("Wynik wynosi: " + val);
+        return true;
+    }
+}
