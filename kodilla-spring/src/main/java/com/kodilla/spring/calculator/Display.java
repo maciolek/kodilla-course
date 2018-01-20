511da2bffe829c09b77c6c605ed92fd78e4a61ad
“Maciolek <maciolek.dariusz@gmail.com>
2018-01-20 18:22:14 +0100

    task10.2 - corected @Configuration

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
-@Configuration
 public class Display {
 
     public boolean displayValue(double val){
