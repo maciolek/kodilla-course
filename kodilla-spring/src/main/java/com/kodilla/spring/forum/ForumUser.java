9492c11349b93a6e9e18bce907d7c7b102726a65
Dariusz <33531485+Maciolek@users.noreply.github.com>
2018-01-21 22:17:18 +0100

    task10.1 - corected PR (#22)
    10.1 - First @Bean ForumUser

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,17 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+
+    private String username;
+
+    public ForumUser() {
+        this.username = "John Smith";
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
