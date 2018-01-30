ef5c14a8ca033bc7329578e82a3b0d757948019f
“Maciolek <maciolek.dariusz@gmail.com>
2018-01-30 01:53:13 +0100

    NEWtask 11.5 complete - BUILDER

--- kodilla-patterns/src/main/java/com/kodilla/patterns/builder/checkers/Pawn.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.patterns.builder.checkers;
-
-public final class Pawn extends Figure {
-    public Pawn(String color) {
-        super(color);
-    }
-}
--- kodilla-patterns/src/main/java/com/kodilla/patterns/builder/checkers/Queen.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.patterns.builder.checkers;
-
-public class Queen extends Figure {
-    public Queen(String color) {
-        super(color);
-    }
-}
