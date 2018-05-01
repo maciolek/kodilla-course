3c16e05cb4a7ebfa0a9d18b15a828d1fd85ba235
“Maciolek <maciolek.dariusz@gmail.com>
2018-05-01 16:03:51 +0200

    add adapter pattern

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdaptee.java
@@ -0,0 +1,23 @@
+package com.kodilla.patterns2.adapter.bookclasifier;
+
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book;
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSatatistic;
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.Statistics;
+
+import java.util.Map;
+
+public class MedianAdaptee implements BookSatatistic {
+
+    Statistics statistics = new Statistics();
+
+    @Override
+    public int averagePublicationYear(Map<BookSignature, Book> books) {
+        return statistics.averagePublicationYear(books);
+    }
+
+    @Override
+    public int medianaPublicationYear(Map<BookSignature, Book> books) {
+        return statistics.medianaPublicationYear(books);
+    }
+}
---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/libraryb/BookSignature.java
@@ -0,0 +1,15 @@
+package com.kodilla.patterns2.adapter.bookclasifier.libraryb;
+
+public class BookSignature {
+
+    private final String sinature;
+
+    public BookSignature(String sinature) {
+        this.sinature = sinature;
+    }
+
+    public String getSinature() {
+        return sinature;
+    }
+
+}
