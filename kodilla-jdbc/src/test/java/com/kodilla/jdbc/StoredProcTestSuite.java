a6c85678d448f68be64f224e784f9fd0ddc1e137
“Maciolek <maciolek.dariusz@gmail.com>
2018-05-06 00:02:43 +0200

    add mod 20.2 SQL procedure from java

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,36 @@
+package com.kodilla.jdbc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class StoredProcTestSuite {
+
+    @Test
+    public void testUpdateVipLevel() throws SQLException {
+
+        //given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //when
+
+        String sqlProcedureCall = "CALL updateVipLevels()";
+        statement.execute(sqlProcedureCall);
+        //then
+
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if (rs.next()){
+            howMany = rs.getInt("HOW_MANY");
+
+            Assert.assertEquals(0,howMany);
+        }
+    }
+}
