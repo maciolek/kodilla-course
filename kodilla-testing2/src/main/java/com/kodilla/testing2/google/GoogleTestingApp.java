e1fb087c9f0eff30e28aa369384a1b6ca0c9e4e2
“Maciolek <maciolek.dariusz@gmail.com>
2018-05-08 15:40:50 +0200

    start selenium

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -0,0 +1,22 @@
+package com.kodilla.testing2.google;
+
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class GoogleTestingApp {
+
+
+public static final String SEARCHINGFIELD ="lst-ib";
+
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.google.com");
+
+        WebElement searchField = driver.findElement(By.id(SEARCHINGFIELD));
+        searchField.sendKeys("Kodilla");
+        searchField.submit();
+    }
+}
